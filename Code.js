a241eb0d15aa32d3f9481a46c79973af7224d179
Mwqqm <50789048+Mwqqm@users.noreply.github.com>
2019-05-20 10:04:34 +0300

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,22 @@
+﻿const Discord = require('discord.js');
+const MooDY = new Discord.Client();
+
+client.on('ready', () => {
+  client.user.setGame(`By | Mo oDY`,'https://www.twitch.tv/iiMooDY9');
+  console.log("["+MooDY.user.username+"] Online now !!");
+});
+MooDY.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ألسـلآم عليـكم 
+
+ياقـلبي خش السيـرفر ذا 
+
+وبكـون شـآكر لكك
+                               [ https://discord.gg/4ntmNUB ] 
+ألـدعوةة ذي خآصةة لك ${member} !
+**`) 
+}).catch(console.error)
+})
+MooDY.login(" "); // <= SharingBot token here !!
